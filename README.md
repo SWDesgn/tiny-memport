8c0ae07a64a486ef5020f6e7bce7a6ce64cedcd6
Oliver Altergott <altergott.oliver@gmail.com>
2020-08-23 17:56:42 +0200

    added readme, added buffer param

---
+++ README.md
@@ -0,0 +1,7 @@
+# tinymemport
+
+C++ Header-only heap-based data allocation + retrieval lib that is thread safe for data communication/sharing between threads. 
+
+## Author
+
+Oliver Altergott
--- include/memport.h
+++ include/memport.h
@@ -1,50 +1,52 @@
 #pragma once
 
+#include <cstdint>
 #include <mutex>
 
-namespace tinymemport
-{
-	template <typename T>
-	class TDataPort
-	{
-	public:
-		static_assert(std::is_default_constructible<T>::value, "DataType must be default-constructible");
-		static_assert(std::is_assignable<T, T>::value, "DataType must be a assignable");
+namespace tinymemport {
+template <typename T>
+class TDataPort {
+ public:
+  static_assert(std::is_default_constructible<T>::value,
+                "DataType must be default-constructible");
+  static_assert(std::is_assignable<T, T>::value,
+                "DataType must be a assignable");
 
-		TDataPort() : m_guard(), m_data_p()
-		{
-			std::lock_guard<std::mutex> lock(m_guard);
-			m_data_p = new (std::nothrow) T;
-		}
-		~TDataPort()
-		{
-			std::lock_guard<std::mutex> lock(m_guard);
-			delete m_data_p;
-		}
+  TDataPort(uint16_t f_bufferBytes) : m_guard(), m_data_p() {
+    std::lock_guard<std::mutex> lock(m_guard);
+    m_rawMem_p = new (std::nothrow) char[sizeof(T) + f_bufferBytes];
+    m_data_p = reinterpret_cast<T *>(m_rawMem_p);
+    if (m_data_p != nullptr) {
+      T t;
+      *m_data_p = t;
+    }
+  }
+  ~TDataPort() {
+    std::lock_guard<std::mutex> lock(m_guard);
+    delete[] m_rawMem_p;
+    m_data_p = nullptr;
+  }
 
-		void setData(const T f_outputData)
-		{
-			std::lock_guard<std::mutex> lock(m_guard);
-			if (m_data_p != nullptr)
-			{
-				*m_data_p = f_outputData;
-			}
-		}
+  void setData(const T f_outputData) {
+    std::lock_guard<std::mutex> lock(m_guard);
+    if (m_data_p != nullptr) {
+      *m_data_p = f_outputData;
+    }
+  }
 
-		bool hasData()
-		{
-			std::lock_guard<std::mutex> lock(m_guard);
-			return (m_data_p != nullptr);
-		}
+  bool hasAlloc() {
+    std::lock_guard<std::mutex> lock(m_guard);
+    return (m_data_p != nullptr);
+  }
 
-		T *getData()
-		{
-			std::lock_guard<std::mutex> lock(m_guard);
-			return m_data_p;
-		}
+  T *getData() {
+    std::lock_guard<std::mutex> lock(m_guard);
+    return m_data_p;
+  }
 
-	private:
-		std::mutex m_guard;
-		T *m_data_p;
-	};
-} // namespace tinymemport
+ private:
+  std::mutex m_guard;
+  char *m_rawMem_p;
+  T *m_data_p;
+};
+}  // namespace tinymemport
